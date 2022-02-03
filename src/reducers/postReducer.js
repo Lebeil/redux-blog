621a93fbf4746c5c3635e2967db2dc17da0f17c2
Liedel <liedel75@gmail.com>
2022-02-03 18:54:47 +0100

    add action

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,5 +1,12 @@
-const initialState = { state: "Hello!" };
+import { GET_POSTS } from "../actions/postAction";
+
+const initialState = {};
 
 export default function postReducer(state = initialState, action) {
-    return initialState;
+    switch(action.type) {
+        case GET_POSTS:
+            return action.payload
+        default:
+            return state;
+    }
 }
