4016cb84dbf8825c470715ea9d98931d5b7e13b5
Liedel <liedel75@gmail.com>
2022-02-03 17:25:23 +0100

    add reducers

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const store = createStore(
   rootReducer,
---
+++ src/reducers/postReducer.js
@@ -0,0 +1,5 @@
+const initialState = { state: "Hello!" };
+
+export default function postReducer(state = initialState, action) {
+    return initialState;
+}
