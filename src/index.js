c1f8a69c32178a013a0529570b3ff6510c87d3e1
Liedel <liedel75@gmail.com>
2022-02-04 11:46:25 +0100

    add isEmpty function

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { getPosts } from './actions/postAction';
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
 
+store.dispatch(getPosts());
 
 ReactDOM.render(
   <React.StrictMode>
