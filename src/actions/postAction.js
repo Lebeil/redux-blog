338e1f99dc44b728c7c09a5c5ef551f88f4c2ff4
Liedel <liedel75@gmail.com>
2022-02-08 21:57:15 +0100

    add form & dispatch

--- src/actions/postAction.js
+++ src/actions/postAction.js
@@ -15,10 +15,10 @@ export const getPosts = () => {
 export const addPosts = (data) => {
     return (dispatch)=> {
         return axios
-        .post('http://localhost:3000/posts', data)
-        .then((res)=> {
-            dispatch({ type: ADD_POSTS, payload: data })
-        })
-        .catch((err)=> console.log(err));
+            .post('http://localhost:3000/posts', data)
+            .then(()=> {
+                dispatch({ type: ADD_POSTS, payload: data })
+            })
+            .catch((err)=> console.log(err));
     };
 };
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addPosts, getPosts } from "../actions/postAction";
 
 const PostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const user = useSelector((state)=> state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleForm = async (e)=> {
+    e.preventDefault();
+
+    if(title && content) {
+      const data = {
+        title,
+        content,
+        author: user[0].pseudo,
+        likes: 0,
+      };
+
+      await dispatch(addPosts(data));
+      setTitle("");
+      setContent("");
+      //pour obtenir l'ID
+      dispatch(getPosts());
+    }
+  }
+
   return (
     <div className="form-container">
-      <form>
-        <input type="text" placeholder="Titre du poste" />
-        <textarea placeholder="Postez vos pensées..."></textarea>
+      <form onSubmit={(e)=> handleForm(e)}>
+        <input 
+          type="text" 
+          placeholder="Titre du poste"
+          value={title}
+          onChange={(e)=> setTitle(e.target.value)} 
+        />
+        <textarea 
+          placeholder="Postez vos pensées..."
+          value={content}
+          onChange={(e)=> setContent(e.target.value)}
+        ></textarea>
         <input type="submit" value="Envoyer" />
       </form>
     </div>
